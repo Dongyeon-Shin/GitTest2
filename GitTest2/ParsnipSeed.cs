6e1b407659528ce61e9e333ae8dbc83c6b9b680d
Username257 <ddw4500@naver.com>
2023-04-17 21:14:37 +0900

    feat: 파스닙을 심는다
    심기가능한 인터페이스를 만들고 그것을 상속받는 파스닙 씨앗 클래스를 만들었다

---
+++ GitTest2/ParsnipSeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest2
+{
+    internal class ParsnipSeed : IPlantable
+    {
+        
+        public void Plant()
+        {
+            Console.WriteLine("파스닙을 심었습니다.");
+        }
+    }
+}
